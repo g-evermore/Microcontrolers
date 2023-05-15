f9a3d822d031fa4e83c72ec7c5580cce35ca5c02
g-evermore <garrett.evermore@protonmail.com>
2023-05-15 17:58:34 -0400

    Updated weather station client and server
    Updated:
    weather_station_client.py
    weather_station_server.py
    
    client has minor changes to print formatting
    server now carries the function record_to_csv() that records information sent from the client to the server in a weather_station_recordings.csv file
    
    [NOTE]:  Later implementations should add column names and use DictReader and DictWriter for csv writer.

--- Weather Station/weather_station_server.py
+++ Weather Station/weather_station_server.py
@@ -1,20 +1,31 @@
 import socket
+import pandas as pd
+import csv
 
 HOST = "127.0.0.1"
 PORT = 1234
+output_filename = 'weather_station_recordings.csv'
+print("Weather Station server stated...")
 
-print("Weather server stated...")
-while(True):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.bind((HOST, PORT))
-        sock.listen()
-        client_socket, client_address = sock.accept()
-        with client_socket:
-            print(f"Sever connected to client at address: {client_address}")
-            while True:
-                client_data = client_socket.recv(1024)
-                client_data_string = client_data.decode()
-                if not client_data:
-                    break
-                server_response = f"SERVER RESPONSE: {client_data_string}"
-                client_socket.sendall(server_response.encode())
+def record_to_csv(environmental_reading):
+     with open(output_filename, 'a', newline='') as output_file:
+        output_writer = csv.writer(output_file)
+        output_writer.writerow(environmental_reading)
+
+try:
+    while(True):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.bind((HOST, PORT))
+            sock.listen()
+            client_socket, client_address = sock.accept()
+            with client_socket:
+                print(f"Sever connected to client at address: {client_address}")
+                while True:
+                    client_data = client_socket.recv(1024)
+                    client_data_string = client_data.decode()
+                    if not client_data:
+                        break
+                    record_to_csv([client_data_string])
+                    server_response = f"SERVER RESPONSE: Recorded {client_data_string}"
+                    client_socket.sendall(server_response.encode())
+except: print("Server closed.")
